04d531f96b5f907ac10c40727be50ab534896ec6
Tatiana Perez <tjperez@ncsu.edu>
2021-03-02 11:04:47 -0500

    fixed error w instructor help

--- core/models.py
+++ core/models.py
@@ -6,22 +6,21 @@ class User(AbstractUser):
     pass
 
 
-
 class Artist(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
     label = models.CharField(max_length=100, blank=True)
-
     def __str__(self):
         return "%s %s" % (self.first_name, self.last_name)
 
+
 class Album(models.Model):
     title = models.CharField(max_length=200)
-    artist = models.CharField(max_length=200, blank=True)
+    #artist = models.CharField(max_length=200, blank=True, null=True)
     release_date = models.DateField()
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, blank=True, null=True, related_name="albums")
-    #artist = models.ForeignKey(Artist, on_delete=models.CASCADE, blank=True, null=True, related_name="artists")
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, blank=True, null=True, related_name="albums")
     #artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     def __str__(self):
         return self.title
